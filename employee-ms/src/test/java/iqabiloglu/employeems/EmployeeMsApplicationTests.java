641d923e0ce723d9227dd298606b173fb9c59f65
Ibrahim <i.qabilogl@gmail.com>
2022-11-06 19:43:01 +0400

    add employee-ms

---
+++ employee-ms/src/test/java/iqabiloglu/employeems/EmployeeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package iqabiloglu.employeems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
