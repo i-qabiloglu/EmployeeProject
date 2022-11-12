12b7b4f35ce37230471dbae9912a6ee0171ebcb3
Ibrahim <i.qabilogl@gmail.com>
2022-11-12 15:56:41 +0400

    add mapper for department

---
+++ employee-ms/src/main/java/iqabiloglu/employeems/mapper/DepartmentMapper.java
@@ -0,0 +1,28 @@
+package iqabiloglu.employeems.mapper;
+
+import iqabiloglu.employeems.dao.entity.DepartmentEntity;
+import iqabiloglu.employeems.model.dto.DepartmentDto;
+import iqabiloglu.employeems.model.view.DepartmentView;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DepartmentMapper {
+
+    public static DepartmentView entityToView(DepartmentEntity entity) {
+        return DepartmentView.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .build();
+    }
+
+    public static DepartmentEntity dtoToEntity(DepartmentDto dto) {
+        return DepartmentEntity.builder()
+                .name(dto.getName())
+                .build();
+    }
+
+    public static Set<DepartmentView> entitiesToViews(Set<DepartmentEntity> entities) {
+        return entities.stream().map(DepartmentMapper::entityToView).collect(Collectors.toSet());
+    }
+}
