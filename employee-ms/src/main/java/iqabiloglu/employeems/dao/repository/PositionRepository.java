4cbdaad7ddbda3e690b97f7725742572769e1700
Ibrahim <i.qabiloglu@gmail.com>
2022-12-17 01:36:45 +0400

    add NotEmptyException class and update Department delete method in service

--- employee-ms/src/main/java/iqabiloglu/employeems/dao/repository/PositionRepository.java
+++ employee-ms/src/main/java/iqabiloglu/employeems/dao/repository/PositionRepository.java
@@ -12,7 +12,11 @@ import java.util.Optional;
 public interface PositionRepository extends JpaRepository<PositionEntity, Long> {
 
     @Query(nativeQuery = true, value = "select * from positions where is_deleted = false")
-    List<PositionEntity> findAllPositions();
+    List<PositionEntity> findAll();
+
+    List<PositionEntity> findAllByIsDeletedFalseAndDepartment_Id(Long departmentId);
 
     Optional<PositionEntity> findByIdAndIsDeletedFalse(Long id);
+
+
 }
--- employee-ms/src/main/java/iqabiloglu/employeems/service/impl/PositionServiceImpl.java
+++ employee-ms/src/main/java/iqabiloglu/employeems/service/impl/PositionServiceImpl.java
@@ -1,25 +1,38 @@
 package iqabiloglu.employeems.service.impl;
 
+import iqabiloglu.employeems.dao.entity.PositionEntity;
+import iqabiloglu.employeems.dao.repository.PositionRepository;
+import iqabiloglu.employeems.mapper.PositionMapper;
 import iqabiloglu.employeems.model.dto.PositionDto;
+import iqabiloglu.employeems.model.exception.NotFoundException;
 import iqabiloglu.employeems.model.view.PositionView;
 import iqabiloglu.employeems.service.PositionService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static iqabiloglu.employeems.model.constant.ExceptionConstants.POSITION_NOT_FOUND_CODE;
+import static iqabiloglu.employeems.model.constant.ExceptionConstants.POSITION_NOT_FOUND_MESSAGE;
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PositionServiceImpl implements PositionService {
+
+    private final PositionRepository repository;
+
 
     @Override
     public List<PositionView> getList() {
-        return null;
+        return PositionMapper.entitiesToViews(repository.findAll());
     }
 
     @Override
     public List<PositionView> getListByDepartment(Long departmentId) {
-        return null;
+
+        return PositionMapper.entitiesToViews(repository.findAllByIsDeletedFalseAndDepartment_Id(departmentId));
     }
 
     @Override
@@ -40,6 +53,13 @@ public class PositionServiceImpl implements PositionService {
     @Override
     public void delete(Long id) {
 
+    }
+
+    PositionEntity fetchIfExist(Long id) {
+        return repository.findByIdAndIsDeletedFalse(id).orElseThrow(() -> {
+            log.error("PositionServiceImpl.fetchIfExist.error id: {}", id);
+            throw new NotFoundException(POSITION_NOT_FOUND_CODE, String.format(POSITION_NOT_FOUND_MESSAGE));
+        });
     }
 
 
