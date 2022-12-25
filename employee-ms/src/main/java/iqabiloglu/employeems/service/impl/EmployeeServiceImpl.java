6ae85291ac64e68d3301d09bd77d2d3d3dbcac69
Ibrahim <i.qabiloglu@gmail.com>
2022-12-25 20:37:21 +0400

    update employee specification

--- employee-ms/src/main/java/iqabiloglu/employeems/service/impl/EmployeeServiceImpl.java
+++ employee-ms/src/main/java/iqabiloglu/employeems/service/impl/EmployeeServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static iqabiloglu.employeems.util.constant.ExceptionConstants.*;
