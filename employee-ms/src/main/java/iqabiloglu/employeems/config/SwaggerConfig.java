34a69ee6d279281f66ded7f6b1b9f3018d40fa40
Ibrahim <i.qabiloglu@gmail.com>
2023-01-21 16:51:13 +0400

    update v1.0

--- employee-ms/src/main/java/iqabiloglu/employeems/config/SwaggerConfig.java
+++ employee-ms/src/main/java/iqabiloglu/employeems/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,22 +15,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        var apiInfo = new ApiInfoBuilder().title("Employee Project")
-                                          .version("1.0")
-                                          .license(null)
-                                          .licenseUrl(null)
-                                          .termsOfServiceUrl(null)
-                                          .description("The Employe project ui to show the integration of " +
-                                                               "swagger ui.")
-                                          .build();
+    public Docket api( ) {
 
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo)
-                                                   .useDefaultResponseMessages(false)
-                                                   .select()
-                                                   .apis(RequestHandlerSelectors.basePackage("iqabiloglu.employeems"))
-                                                   .paths(PathSelectors.regex("/.*"))
-                                                   .build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                                                      .useDefaultResponseMessages(false)
+                                                      .select()
+                                                      .apis(RequestHandlerSelectors.basePackage(
+                                                              "iqabiloglu.employeems"))
+                                                      .paths(PathSelectors.any())
+                                                      .build();
+    }
+
+    private ApiInfo apiInfo( ) {
+        return new ApiInfoBuilder().
+                title("Employee Project")
+                .version("1.0")
+                .description("The Employee project ui to show the integration of swagger ui.")
+                .build();
     }
 
 
